6facd3d2702592bf25e23b4042cfab37c93c933a
matteopierro <test@test.com>
2019-01-02 15:29:14 +0100

    extract run method injecting the random number generator

--- java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
@@ -1,14 +1,14 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
-
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.Test;
+
+import java.util.Random;
 
 public class GameRunnerTest {
 
 	@Test
 	public void true_is_true() throws Exception {
-		GameRunner.main(new String[]{});
+		GameRunner.run(new Random(3));
 	}
 }
