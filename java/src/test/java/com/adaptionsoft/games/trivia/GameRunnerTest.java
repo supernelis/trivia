29c8aa04c918e07ce4554d6c2e76d291af9e3f77
matteopierro <test@test.com>
2019-01-03 16:32:34 +0100

    adds test with different players combination

--- java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerTest.java
@@ -3,23 +3,43 @@ package com.adaptionsoft.games.trivia;
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 import com.adaptionsoft.games.uglytrivia.Game;
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Random;
 
-import static java.util.Arrays.asList;
-
 public class GameRunnerTest {
 
 	@Test
 	public void can_run_a_game() throws Exception {
 		Integer[] seeds = new Integer[]{3, 5};
-		Approvals.verifyAll(seeds, this::runGame);
+		Approvals.verifyAll(seeds, seed -> runGame(seed, new Players("Chet", "Pat", "Sue")));
 	}
 
-	private String runGame(int seed) {
+	@Test
+	public void can_run_a_game_with_different_number_of_players() throws Exception {
+		Integer[] seeds = new Integer[]{3, 5};
+		Players[] playersCombination = new Players[] {
+				new Players(),
+				new Players("Chet"),
+				new Players("Chet", "Pat"),
+				new Players("Chet", "Pat", "Sue"),
+				new Players("Chet", "Pat", "Sue", "Fizz"),
+				new Players("Chet", "Pat", "Sue", "Fizz", "Buzz"),
+				new Players("Chet", "Pat", "Sue", "Fizz", "Buzz", "Foo")
+		};
+
+		CombinationApprovals
+				.verifyAllCombinations(
+						this::runGame,
+						seeds,
+						playersCombination
+				);
+	}
+
+	private String runGame(int seed, Players players) {
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		Game game = new Game() {
 			@Override
@@ -27,8 +47,6 @@ public class GameRunnerTest {
 				return new PrintStream(byteArrayOutputStream, true);
 			}
 		};
-
-		Players players = new Players("Chet", "Pat", "Sue");
 
 		GameRunner.run(new Random(seed), game, players.values());
 
