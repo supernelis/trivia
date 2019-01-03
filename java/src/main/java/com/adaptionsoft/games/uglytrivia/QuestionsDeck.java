59c7e34caf2cf82e631a078a53e84341cb39fba1
matteopierro <test@test.com>
2019-01-03 18:44:59 +0100

    refactor QuestionsDeck

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionsDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionsDeck.java
@@ -2,36 +2,34 @@ package com.adaptionsoft.games.uglytrivia;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static com.adaptionsoft.games.uglytrivia.Category.*;
-import static com.adaptionsoft.games.uglytrivia.Category.ROCK;
+import static com.adaptionsoft.games.uglytrivia.Category.categoryFor;
+import static com.adaptionsoft.games.uglytrivia.Category.values;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.IntStream.range;
 
 public class QuestionsDeck {
 
+    private static final int QUESTIONS_PER_CATEGORY = 50;
     private final HashMap<Category, LinkedList<String>> questionsForCategory;
 
     public QuestionsDeck() {
-        LinkedList<String> popQuestions = new LinkedList<>();
-        LinkedList<String> scienceQuestions = new LinkedList<>();
-        LinkedList<String> sportsQuestions = new LinkedList<>();
-        LinkedList<String> rockQuestions = new LinkedList<>();
-
-        for (int i = 0; i < 50; i++) {
-            popQuestions.addLast("Pop Question " + i);
-            scienceQuestions.addLast("Science Question " + i);
-            sportsQuestions.addLast("Sports Question " + i);
-            rockQuestions.addLast("Rock Question " + i);
-        }
-
         questionsForCategory = new HashMap<>();
-        questionsForCategory.put(POP, popQuestions);
-        questionsForCategory.put(SCIENCE, scienceQuestions);
-        questionsForCategory.put(SPORTS, sportsQuestions);
-        questionsForCategory.put(ROCK, rockQuestions);
+        for (Category category : values()) {
+            questionsForCategory.put(category, makeQuestionsFor(category));
+        }
     }
 
     public String pickQuestionFor(int position) {
         LinkedList<String> currentQuestions = questionsForCategory.get(categoryFor(position));
         return currentQuestions.removeFirst();
+    }
+
+    private LinkedList<String> makeQuestionsFor(Category category) {
+        return range(0, QUESTIONS_PER_CATEGORY).boxed()
+                .map(questionNumber -> category.value() + " Question " + questionNumber)
+                .collect(toCollection(LinkedList::new));
     }
 }
